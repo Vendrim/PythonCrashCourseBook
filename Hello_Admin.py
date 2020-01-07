514bd47c0c844485eb9a2cfe211a7425f1ec9b56
vbialek <Victor.Bialek@hs-augsburg.de>
2020-01-07 20:19:50 +0100

    Exercise 5.9 from the Book has been finished

--- Hello_Admin.py
+++ Hello_Admin.py
@@ -1,10 +1,14 @@
-username_list = ["John", "Victor", "Kolya", "Sarah", "admin"]
-input = input("Username: ")
-if (input in username_list):
-    if(input == "admin"):
-        print(f' Welcome Back {input}. Would you like to see a usage report?')
+username_list = []
+username_list2 = ["John", "Victor", "Kolya", "Sarah", "admin"]
+if username_list:
+    print(f'We need to find some Users!')
+    input = input("Username: ")
+    if (input in username_list):
+        if(input == "admin"):
+            print(f' Welcome Back {input}. Would you like to see a usage report?')
+        else:
+            print(f'Welcome Back {input}. Enjoy your Session.')
     else:
-        print(f'Welcome Back {input}.')
+        print(f'User was not found.')
 else:
-    print(f'User was not found.')
-
+    print(f"There are no Users in our Database!")

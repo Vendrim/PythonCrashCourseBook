7e477b7f748e07cd96fe18fe683ad2c05147ed02
vbialek <Victor.Bialek@hs-augsburg.de>
2020-01-08 20:13:22 +0100

    Exercise 6-2. added.

---
+++ Chapter 6/Favorite_Numbers.py
@@ -0,0 +1,20 @@
+People = {
+    'Rodney': '42',
+    'John': '13',
+    'Felix': '27',
+    'Kolya': '34',
+    'Victor' : '23'
+}
+
+name1 = People['Rodney']
+name2 = People['John']
+name3 = People['Felix']
+name4 = People['Kolya']
+name5 = People['Victor']
+
+
+print(f'Rodney: {name1}')
+print(f'John: {name2}')
+print(f'Felix: {name3}')
+print(f'Kolya: {name4}')
+print(f'Victor: {name5}')

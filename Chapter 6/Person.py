6296b9718f88e151b1de506693f7bbfd48a1c456
vbialek <Victor.Bialek@hs-augsburg.de>
2020-01-08 10:55:20 +0100

    Chapter folders added

---
+++ Chapter 6/Person.py
@@ -0,0 +1,13 @@
+Person = {
+    'first name': 'Victor',
+    'last name':'Bialek',
+    'age': '23.6',
+    'city': 'Augsburg',
+}
+f = Person['first name']
+l = Person['last name']
+a = Person['age']
+c = Person['city']
+
+
+print(f' Name: {f} {l}  Age: {a} City: {c}')

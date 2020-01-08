120cf2262c0b26be0435cd3a320cb30642b35780
vbialek <Victor.Bialek@hs-augsburg.de>
2020-01-08 20:21:21 +0100

    Exercise 6.3 added

---
+++ Chapter 6/Glossary.py
@@ -0,0 +1,15 @@
+Glossary = {
+    'list': 'A set of values.',
+    'string': 'A cooler way of saying text',
+    'dictionary': 'Its sort of like a map in Java, but better.',
+    'argument': 'A parameter/variable from the outside (I guess)',
+    'loop': 'A data flow control element, which goes through commands until a specific condition is fulfilled.'
+
+}
+print("Stuff I learned while learning this:")
+print('\n')
+print('List: ' + Glossary['list'])
+print('String: ' + Glossary['string'])
+print('Dictionary: ' + Glossary['dictionary'])
+print('Argument: ' + Glossary['argument'])
+print('Loop: ' + Glossary['loop'])
